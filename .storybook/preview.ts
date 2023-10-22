e199e7578226a9fea85d33dde1efb9231028b55f
sazanami5 <hbws001@gmail.com>
2023-10-23 02:14:31 +0900

    git init

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from "@storybook/react";
+import {withScreenshot} from "storycap"
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const decorators = [withScreenshot];
+
+export default preview;
